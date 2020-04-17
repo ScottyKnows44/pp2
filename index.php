4c7183909ee1b3a88d67122cf20cf87da868a4c1
Scott thompson <Sthomspon42@mail.greenriver.edu>
2020-04-17 19:00:22 -0400

    added index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*Scott Thompson
+  4/17/2020
+  sthompson.greenriverdev.com/328/index.php
+  Pair Program
+*/
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+echo "PHP Array Practice";
+
